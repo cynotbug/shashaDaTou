94be3b6f30368d0cca86e1f52b2fdb7a6c7fd5f9
ycnotbug <8340934+ycnotbug@user.noreply.gitee.com>
2023-12-15 16:33:36 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import legacy from "@vitejs/plugin-legacy";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ["defaults", "not IE 11"]
+    })
+  ],
   base: "/5114/",
-  publicPath: "./",
+  // publicPath: "./",
   resolve: {
     alias: [
       {
